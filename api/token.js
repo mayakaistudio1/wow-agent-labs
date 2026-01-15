2d0c18aae08b07ea01145959070641ed12d06651
mayakaistudio1 <mayak.aistudio1@gmail.com>
2026-01-15 22:24:20 +0100

    Update token.js

--- api/token.js
+++ api/token.js
@@ -9,25 +9,18 @@ export default async function handler(req, res) {
       return res.status(500).json({ error: "Missing LIVEAVATAR_API_KEY in Vercel env" });
     }
 
-    // твои значения
+    // фиксированные значения
     const AVATAR_ID = "9650a758-1085-4d49-8bf3-f347565ec229";
     const VOICE_ID  = "c23719ef-d070-42ee-9cd9-4b867c621671";
+    const CONTEXT_ID= "5a1d3940-21c0-415d-a549-da0c8bbee5d3";
 
     const payload = {
       mode: "FULL",
       avatar_id: AVATAR_ID,
       avatar_persona: {
         voice_id: VOICE_ID,
-        language: "auto", // автоматическое распознавание речи
-        persona_prompt: `
-          You are a real-time interpreter between Russian and English.
-          Automatically detect which language the speaker is using.
-          If speech is in Russian — immediately translate into English.
-          If speech is in English — immediately translate into Russian.
-          Use fluent, natural, conversational tone. Be fast and accurate.
-          Never comment, explain, or ask questions. Only translate the spoken words.
-          Do not repeat. Do not summarize. Do not initiate or continue conversations.
-        `
+        context_id: CONTEXT_ID,
+        language: "auto"
       }
     };
 
